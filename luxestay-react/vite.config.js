2a8f16389ac82dcbdaf422dc559acfed344643c9
nikitha-08n <nikithaareddy805@gmail.com>
2026-05-29 23:07:59 +0530

    Update app: fix script tags, add deploy config, and latest changes

--- luxestay-react/vite.config.js
+++ luxestay-react/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/luxestays/',
   plugins: [react()],
   server: {
     host: 'localhost',
